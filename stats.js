7b7b423986374ab42f48512fa6a968f2d8f88db6
Pocket Pistol <pocketpistol556@gmail.com>
2025-12-30 18:49:50 -0500

    Add weapon stats for melee, ranged, and projectile
    This file contains stats for melee, ranged, and projectile weapons, including their abilities and cool stats.

---
+++ stats.js
@@ -0,0 +1,143 @@
+
+/* 
+---------------------------------------------------------------------------------------------------------------------------------------
+MELEE WEAPON STATS
+---------------------------------------------------------------------------------------------------------------------------------------
+*/
+const meleeNum = [
+    " Damage", 
+    " Range", 
+    " Swing Speed"
+];
+
+const meleeCool = [
+  "on miss: hit yourself, idiot",
+  "Crit vs. Burning Players",
+  "Crit vs. Non-Burning Players",
+  "Crit vs. Wet Players",
+  "Crit vs. Dry Players",
+  "Crit vs. Bleeding Players",
+  "Crit vs. Non-bleeding Players",
+  "Crit vs. Overhealed Players",
+  "Crit vs. Players Covered In Milk",
+  "Crit vs. Players Covered In Jarate",
+  "Mini-Crit vs. Burning Players",
+  "Mini-Crit vs. Non-Burning Players",
+  "Mini-Crit vs. Wet Players",
+  "Mini-Crit vs. Dry Players",
+  "Mini-Crit vs. Bleeding Players",
+  "Mini-Crit vs. Non-bleeding Players",
+  "Mini-Crit vs. Overhealed Players",
+  "Mini-Crit vs. Players Covered In Milk",
+  "Mini-Crit vs. Players Covered In Jarate",
+  "On hit: Inflict afterburn",
+  "On hit: Inflict Wet status",
+  "On hit: Inflict Overheal",
+  "On hit: Inflict Marked-For-Death",
+  "On hit: Inflict Reversed Controls",
+  "On hit: Inflict Afterburn",
+  "On hit: Inflict Cough Virus",
+  "No Random Critical Hits"
+];
+
+const meleeAbility = [
+    "Collect heads from defeated enemies",
+    "Gain health on hit.",
+    "Heal back your opponents max health on kill",
+    "Gain overheal on kill",
+    "Gain a speed boost on kill",
+    "Gain a damage boost on kill",
+    "Gain a swing speed boost on kill",
+    "Gain a full stat boost on kill",
+    "Crits on kill"
+];
+
+const meleeRare = [
+    "Become uber'd on kill",
+    "Start flying on kill",
+    "8s of bleed on hit"
+    ];
+    
+/* 
+---------------------------------------------------------------------------------------------------------------------------------------
+RANGED WEAPON STATS
+---------------------------------------------------------------------------------------------------------------------------------------
+*/
+
+const gunStats = {
+  stats: [
+    " Attack Speed",
+    " Damage",
+    " Range",
+    " Firing Speed",
+    " Projectile Speed",
+    " Projectile Amount",
+    " Projectile Damage",
+    " Reload Speed",
+    " Clip size"
+  ],
+
+  abilities: [
+    "Collect Heads from your enemies."
+  ],
+
+  coolStats: [
+    "Mini-Crits vs. Players Marked For Death",
+    "Mini-Crits vs. Burning Players",
+    "Mini-Crits vs. Non-Burning Players",
+    "Mini-Crits vs. Wet Players",
+    "Mini-Crits vs. Dry Players",
+    "Mini-Crits vs. Bleeding Players",
+    "Mini-Crits vs. Non-bleeding Players",
+    "Mini-Crits vs. Overhealed Players",
+    "Mini-Crits vs. Players Covered In Milk",
+    "Mini-Crits vs. Players Covered In Jarate",
+    "On hit: Inflict afterburn",
+    "On hit: Inflict wet status",
+    "On hit: Inflict Overheal",
+    "On hit: Inflict Marked-For-Death",
+    "No Random Critical Hits"
+  ]
+};
+/* 
+---------------------------------------------------------------------------------------------------------------------------------------
+PROJECTILE WEAPON STATS
+---------------------------------------------------------------------------------------------------------------------------------------
+*/
+const projectileStats = {
+  stats: [
+    " Attack Speed",
+    " Damage",
+    " Range",
+    " Firing Speed",
+    " Projectile Speed",
+    " Projectile Amount",
+    " Projectile Damage",
+    " Reload Speed",
+    " Clip size",
+    " Explosion Size",
+    " Projectile Lifetime"
+  ],
+
+  abilities: [
+    "Collect Heads from your enemies."
+  ],
+
+  coolStats: [
+    "Mini-Crits vs. Players Marked For Death",
+    "Mini-Crits vs. Burning Players",
+    "Mini-Crits vs. Non-Burning Players",
+    "Mini-Crits vs. Wet Players",
+    "Mini-Crits vs. Dry Players",
+    "Mini-Crits vs. Bleeding Players",
+    "Mini-Crits vs. Non-bleeding Players",
+    "Mini-Crits vs. Overhealed Players",
+    "Mini-Crits vs. Players Covered In Milk",
+    "Mini-Crits vs. Players Covered In Jarate",
+    "On hit: Inflict afterburn",
+    "On hit: Inflict wet status",
+    "On hit: Inflict Overheal",
+    "On hit: Inflict Marked-For-Death",
+    "No Random Critical Hits"
+  ]
+};
