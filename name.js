46f10903b4a87e65d80118c79add2143b6633bc1
Pocket Pistol <pocketpistol556@gmail.com>
2025-12-30 18:54:38 -0500

    Add nameNoun and nameVrb arrays in name.js

---
+++ name.js
@@ -0,0 +1,27 @@
+const nameNoun = [
+  "Heavy",
+  "apple",
+  "mountain",
+  "bicycle",
+  "library",
+  "river",
+  "candle",
+  "astronaut",
+  "suitcase",
+  "mirror",
+  "thunder"
+];
+
+const nameVrb = [
+  "Hitter",
+  "wander",
+  "linger",
+  "deliver",
+  "gather",
+  "whisper",
+  "consider",
+  "discover",
+  "remember",
+  "answer",
+  "enter"
+];
