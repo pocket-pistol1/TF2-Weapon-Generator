7517edaf3e3a8860c2b80f2c1d2aee45126f1eea
Pocket Pistol <pocketpistol556@gmail.com>
2025-12-30 19:12:58 -0500

    Create generator.js

---
+++ generator.js
@@ -0,0 +1,10 @@
+import {meleeNum, meleeCool, meleeRare, 
+rangedstats, rangedabilites, rangedcoolStats, 
+projstats, projabilites, projcoolstats} from 'stats.js';
+
+class generator {
+
+    get_melee_numstats() {
+        
+    }
+}
